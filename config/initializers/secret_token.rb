b2f9359227cb5f269a081a5f6dccedc1abd712d8
Kristen Hazard <kristen@hazardbio.com>
2014-02-27 13:52:26 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shapeshifter::Application.config.secret_key_base = '1868671e471828adf1361ddf6e3f3b01b4b92fa3e66af787e5000387f827ffa0645179253680834e34d97b6030748019dec3bff80a52b14ac32d6addd3ff9616'
