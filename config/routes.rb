95366e81a99a0c221b5b016630759a57d6eb0db1
Kristen Hazard <kristen@hazardbio.com>
2014-02-27 18:26:23 -0800

    hanuman root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Shapeshifter::Application.routes.draw do
   get "home/index"
+  get "home", to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
