64dd5a2d379aa6cc7f7a7080fc6f75d547a0960e
Kristen Hazard <kristen@hazardbio.com>
2014-03-17 13:49:19 -0700

    add observation_answer migration to shapeshifter

---
+++ db/migrate/20140314223544_create_hanuman_observation_answers.hanuman.rb
@@ -0,0 +1,12 @@
+# This migration comes from hanuman (originally 20140313234939)
+class CreateHanumanObservationAnswers < ActiveRecord::Migration
+  def change
+    create_table :hanuman_observation_answers do |t|
+      t.integer :observation_id
+      t.text :multi_answer
+      t.integer :answer_choice_id
+
+      t.timestamps
+    end
+  end
+end
